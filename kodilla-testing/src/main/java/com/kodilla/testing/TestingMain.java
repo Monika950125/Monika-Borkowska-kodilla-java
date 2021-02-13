835fe494bb88a77fd4b7ad8d9f529da2f2bddc7d
Monika Borkowska <monikademianiuk@gmail.com>
2021-02-13 15:43:09 +0100

    6.2 Added Calculator class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(15,8);
+        int calculation1 = calculator.subtract();
+
+        if(calculation1 == calculator.getA() - calculator.getB()) {
+            System.out.println("Subtraction test OK");
+        }else {
+            System.out.println("Subtraction error");
+        }
+
+        int calculation2 = calculator.add();
+
+        if(calculation2 == calculator.getA() + calculator.getB()) {
+            System.out.println("Addition test OK");
+        }else {
+            System.out.println("Addition error");
+        }
     }
 }
