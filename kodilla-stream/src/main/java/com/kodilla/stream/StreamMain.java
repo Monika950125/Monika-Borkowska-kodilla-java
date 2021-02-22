7a799789138b1b554275496ac746173772e84646
Monika Borkowska <monikademianiuk@gmail.com>
2021-02-22 19:55:17 +0100

    Tasks 7.2, 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,27 +1,25 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        Processor processor = new Processor();
-        processor.execute(() -> System.out.println("This is an example text!"));
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(10, 5, FunctionalCalculator:: subBFromA);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> theResultMapOfUsers = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getBirthDate().getYear() <= 2001)
+                .filter(forumUser -> forumUser.getQuantityOfPublishedPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("castle", String::toUpperCase);
-        poemBeautifier.beautify("FLOWER", (text) -> text.toLowerCase());
-        poemBeautifier.beautify("castle", (text) -> "sand " + text);
-        poemBeautifier.beautify("castle", (text) -> text.substring(0, 4));
-        poemBeautifier.beautify("castle", (text) -> text.replace("a","A"));
+        theResultMapOfUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ":" + entry.getValue())
+                .forEach(System.out::println);
 
     }
 }
