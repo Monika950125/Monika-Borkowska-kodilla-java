13196f02f6e3d31902566d193713107008007d0c
Monika Borkowska <monikademianiuk@gmail.com>
2021-09-13 18:00:30 +0200

    Task 30.2

---
+++ kodilla-testing2/src/main/java/google/GoogleTestingApp.java
@@ -0,0 +1,8 @@
+package google;
+
+public class GoogleTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "c:\\Selenium-drivers\\Firefox\\geckodriver.exe");    // [1]
+        System.setProperty("webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+    }
+}
