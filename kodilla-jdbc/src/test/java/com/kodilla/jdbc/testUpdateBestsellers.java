070ba982cffc300f567a0446660aaae4cd93a0e7
Monika Borkowska <monikademianiuk@gmail.com>
2021-09-13 18:42:29 +0200

    Task 30.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/testUpdateBestsellers.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/testUpdateBestsellers.java
@@ -8,30 +8,32 @@ import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StoredProcTestSuite {
+public class testUpdateBestsellers {
+
     @Test
-    public void testUpdateVipLevels() throws SQLException {
+    public void testUpdateBestsellers() throws SQLException {
         // Given
         DbManager dbManager = DbManager.getInstance();
-        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER= null ";
         Statement statement = dbManager.getConnection().createStatement();
         statement.executeUpdate(sqlUpdate);
-        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
-
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER= null";
 
         // When
-        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
         statement.execute(sqlProcedureCall);
         ResultSet rs = statement.executeQuery(sqlCheckTable);
 
-        // Then
+        //Then
         int howMany = -1;
         if (rs.next()) {
             howMany = rs.getInt("HOW_MANY");
         }
 
+        assertEquals(0, howMany);
+
         rs.close();
         statement.close();
-        assertEquals(0, howMany);
+
     }
 }
