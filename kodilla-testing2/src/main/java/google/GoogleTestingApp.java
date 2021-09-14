2c234b3adfefca73121d41ef490e947778160915
Monika Borkowska <monikademianiuk@gmail.com>
2021-09-14 19:20:41 +0200

    Task 30.5

--- kodilla-testing2/src/main/java/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/google/GoogleTestingApp.java
@@ -1,8 +1,11 @@
 package google;
 
+import config.WebDriverConfig;
+import org.openqa.selenium.WebDriver;
+
 public class GoogleTestingApp {
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "c:\\Selenium-drivers\\Firefox\\geckodriver.exe");    // [1]
-        System.setProperty("webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com");
     }
 }
